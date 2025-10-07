0ce1790c31060eebbba5f46e24126552e8adc23e
Armando Ramos Corona <conrat.82@gmail.com>
2025-10-07 02:17:50 -0600

    subida

---
+++ SumaDeCalificaciones.java
@@ -0,0 +1,7 @@
+public class SumaDeCalificaciones{
+    public static void main(String[] args) {
+        system.out.println("Suma de calificaciones");
+        int calificacion1 = 8;
+        
+    }
+}
