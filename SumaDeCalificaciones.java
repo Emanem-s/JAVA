dbc1013d1484cc20740c3e2706ee7fd78e997677
Armando Ramos Corona <conrat.82@gmail.com>
2025-10-07 02:18:45 -0600

    correcto

--- SumaDeCalificaciones.java
+++ SumaDeCalificaciones.java
@@ -2,6 +2,9 @@ public class SumaDeCalificaciones{
     public static void main(String[] args) {
         system.out.println("Suma de calificaciones");
         int calificacion1 = 8;
-        
+        int calificacion2 = 7;
+        int calificacion3 = 9;  
+        int suma = calificacion1 + calificacion2 + calificacion3;
+        System.out.println("La suma de las calificaciones es: " + suma);
     }
 }
